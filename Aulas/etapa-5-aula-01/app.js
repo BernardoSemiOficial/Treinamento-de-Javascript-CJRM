98d2279f10363c807703f467908aece0a1b80b01
Bernardo Pereira Oliveira <bernardo.pereira@foster.com.br>
2021-03-05 22:19:50 -0300

    Teoria sobre DOM - Document Object Model

---
+++ Aulas/etapa-5-aula-01/app.js
@@ -0,0 +1,23 @@
+
+// DOM - Document Object Model
+
+console.log(document);
+
+/* 
+    <!DOCTYPE html>
+    <html lang=​"pt-br">
+        ​<head>​…​</head>
+        ​<body>​…​</body>​
+    </html>​
+*/
+
+// Através do document podemos ter acesso a árvore de elementos da página.
+// Como o body, head, html etc.
+// É chamado de árvore pois qualquer elemento em tela é descende de um elemento pai.
+// Dando uma visão de vários galhos de uma árvore.
+
+// Uma forma de pegarmos um elemento é com querySelector('seletor');
+
+console.log(document.querySelector('body'));
+
+
