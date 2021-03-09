1d9631af27d715a0fd734462af27c6b6e5bf8b14
Bernardo Pereira Oliveira <bernardo.pereira@foster.com.br>
2021-03-09 09:47:12 -0300

    Teoria sobre DOM - Document Object Model - Obter elementos do DOM

--- Aulas/etapa-5-aula-01/app.js
+++ Aulas/etapa-5-aula-01/app.js
@@ -1,7 +1,11 @@
 
+
 // DOM - Document Object Model
 
-console.log(document);
+// O DOM é uma cópia do HTML da página em que podemos manipula-la
+// e assim afetar o HTML em página.
+
+// console.log(document);
 
 /* 
     <!DOCTYPE html>
@@ -18,6 +22,39 @@ console.log(document);
 
 // Uma forma de pegarmos um elemento é com querySelector('seletor');
 
-console.log(document.querySelector('body'));
+// console.log(document.querySelector('body'));
 
 
+// querySelector e querySelectorAll
+
+// const paragrafo = document.querySelector('p');
+
+// const paragrafos = document.querySelectorAll('p');
+
+// const erroDiv = document.querySelector('div.error');
+
+// const erros = document.querySelectorAll('.error');
+
+
+// console.log(paragrafo, paragrafos);
+// // <p>​Hello world!​</p>​ NodeList(3) [p, p, p.error]
+
+// console.log(erroDiv, erros);
+// // <div class=​"error">​Outra mensagem de erro​</div>​ NodeList(2) [p.error, div.error]
+
+
+// Obter um elemento através do ID
+const title = document.getElementById('title');
+
+console.log(title);
+// <h1 id=​"title">​Query selector & query selector all​</h1>​
+
+const erros = document.getElementsByClassName('error');
+
+console.log(erros);
+// HTMLCollection(2) [p.error, div.error]
+
+const paragrafos = document.getElementsByTagName('p');
+
+console.log(paragrafos);
+// HTMLCollection(3) [p, p, p.error]
