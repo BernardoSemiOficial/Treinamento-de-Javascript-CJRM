f679bcb3847388f7b2acc06f6c22cf29c0514af0
Bernardo P <bernardo@usebob.com.br>
2021-07-07 22:50:31 -0300

    Conteúdo sobre gravar e remover dados do firestore

--- Aulas/etapa-15-aula-01/app.js
+++ Aulas/etapa-15-aula-01/app.js
@@ -1,22 +1,24 @@
 const listGames = document.querySelector("[data-list='games']");
+const formAddGame = document.querySelector("[data-js='add-game-form']");
 
 const db = firebase.firestore();
 
 db.collection("games").get()
     .then(snapshot => {
-        // console.log(snapshot)
-        // snapshot.docs.forEach(doc => console.log(doc.data()))
-        snapshot.docs.reduce((acc, doc) => {
+         console.log(snapshot)
+        //snapshot.docs.forEach(doc => console.log(doc.data()))
+        snapshot.docs.reduce((acc, doc, index) => {
 
             const { title, developedBy, createdAt } = doc.data();
 
             acc += `
-                <li class="my-4">
+                <li id="${doc.id}" data-id="${index}" class="my-4">
                     <h5>${title}</h5>
                     <ul>
                         <li>Desenvolvido por ${developedBy}</li>
                         <li>Adicionado no banco em ${new Date(createdAt.toDate()).toLocaleDateString("pt-br")}</li>
                     </ul>
+                    <button data-remove="${index}" class="btn btn-danger btn-sm">Remover</button>
                 </li>
             `
 
@@ -28,3 +30,42 @@ db.collection("games").get()
     .catch(error => {
         console.log(error.message)
     })
+
+formAddGame.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const currentTarget = e.target;
+    const title = currentTarget.title.value;
+    const developer = currentTarget.developer.value;
+
+    const data = {
+        title,
+        developedBy: developer,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    }
+
+    db.collection("games").add(data)
+        .then(snapshot => console.log(snapshot))
+        .catch(error => console.log(error.message))
+})
+
+listGames.addEventListener("click", (e) => {
+
+    const currentTarget = e.target;
+
+    if(!currentTarget.dataset.remove) {
+        return;
+    }
+
+    const itemId = currentTarget.dataset.remove;
+
+    const item = document.querySelector(`[data-id='${itemId}']`);
+    
+    db.collection("games").doc(item.id).delete()
+        .then(snapshot => {
+            console.log(snapshot);
+            item.remove();
+        })
+        .catch(error => error.message)
+
+})
